fa5bc387f589ed4e01efd66dc2c9b71887d95a07
pocin <nemeth.robin@gmail.com>
2018-03-21 15:12:03 +0100

    refactor running tests and travis

--- run_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-IMG=pocin/kbc-ex-gmail-attachments:dev
-docker build -t ${IMG} .
-source .env
-docker run -e KBC_EX_CLIENT_ID -e KBC_EX_CLIENT_SECRET -e KBC_EX_REFRESH_TOKEN ${IMG} python3 -m 'pytest'
-
