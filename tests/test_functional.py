dc98c57ef9e11be90bbe55ebea0fb84036ae384b
Robin Nemeth <nemeth.robin@gmail.com>
2019-05-16 14:27:16 +0200

    #6 duplicated filenames (#7)
    * Handle duplicated attachment filenames

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,14 +1,15 @@
 import pytest
+import re
 import logging
 from main import main, AttachmentsExtractor
 import os
 
-@pytest.mark.skip
+@pytest.mark.skipif(os.getenv('CI', True), reason='CI')
 def test_main_downloading_with_processors(tmpdir, auth_data):
     params = {
         "queries": [
             {
-                "q": "kbc-ex-gmail-attachments has:attachment",
+                "q": "(gmail extractor test normal attachment) has:attachment",
                 "needs_processors": True
             }
         ]
@@ -17,14 +18,14 @@ def test_main_downloading_with_processors(tmpdir, auth_data):
     # expecxt the file in out/files
     downloaded_files = os.listdir(os.path.join(tmpdir.strpath, 'out/files'))
     assert len(downloaded_files) == 1
-    assert downloaded_files[0] == 'test.csv'
+    assert downloaded_files[0] == 'one.csv'
 
-@pytest.mark.skip
+@pytest.mark.skipif(os.getenv('CI', True), reason='CI')
 def test_main_downloading_without_processors(tmpdir, auth_data):
     params = {
         "queries": [
             {
-                "q": "kbc-ex-gmail-attachments has:attachment",
+                "q": "(gmail extractor test normal attachment) has:attachment",
                 "needs_processors": False
             }
         ]
@@ -33,11 +34,11 @@ def test_main_downloading_without_processors(tmpdir, auth_data):
     # expecxt the file in out/files since needs_processors = False
     downloaded_files = os.listdir(os.path.join(tmpdir.strpath, 'out/tables'))
     assert len(downloaded_files) == 1
-    assert downloaded_files[0] == 'test.csv'
+    assert downloaded_files[0] == 'one.csv'
 
 
 
-@pytest.mark.skip
+@pytest.mark.skipif(os.getenv('CI', True), reason='CI')
 def test_ValueError_if_query_doesnt_match_any_message(auth_data, tmpdir, caplog):
     """Test if pagination is necessary"""
     ex = AttachmentsExtractor(**auth_data)
@@ -50,3 +51,38 @@ def test_ValueError_if_query_doesnt_match_any_message(auth_data, tmpdir, caplog)
             outdir.strpath)
 
     assert excinfo.match(r"^No messages matching")
+
+
+@pytest.mark.skipif(os.getenv('CI', True), reason='CI')
+def test_raise_error_on_duplicated_attachment_fielnames_without_processors(auth_data, tmpdir):
+
+    params = {
+        "queries": [
+            {
+                "q": "(gmail extractor test duplicated_attachments) has:attachment",
+                "needs_processors": False
+            }
+        ]
+    }
+    with pytest.raises(FileExistsError) as excinfo:
+        main(params, datadir=tmpdir.strpath, **auth_data)
+        # expecxt the file in out/files since needs_processors = False
+    assert excinfo.match(r"^Attachment '")
+
+@pytest.mark.skipif(os.getenv('CI', True), reason='CI')
+def test_renaming_duplicated_attachments_with_processors(auth_data, tmpdir):
+
+    params = {
+        "queries": [
+            {
+                "q": "(gmail extractor test duplicated_attachments) has:attachment",
+                "needs_processors": True
+            }
+        ]
+    }
+    main(params, datadir=tmpdir.strpath, **auth_data)
+
+    downloaded_files = os.listdir(os.path.join(tmpdir.strpath, 'out/files'))
+    assert len(downloaded_files) == 2
+    for f in downloaded_files:
+        assert f.startswith('same') and f.endswith('.csv')
